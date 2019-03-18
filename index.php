47414b209681a8a2568e02693710f43d0c0fe34d
stefrvg <PS181901@edu.summacollege.nl>
2019-03-18 13:04:35 +0100

    eerste website backup

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include "include/nav.html";
+?>
+
+<body>
+
+<?php
+include "include/header.html";
+?>
+<div class="container container-extrapadding">
+    <div class="row top15">
+        <div class="col-sm-12">
+            <div class="embed-responsive embed-responsive-16by9 bottom15">
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/U_jvd5XABSo?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen=""></iframe>
+            </div>
+        </div>
+    </div>
+    <br/>
+    <br/>
+</div>
+
+  <?php
+  include "include/footer.html";
+  ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+
+</html>
