7cbbde35b732c62acec82b12c92feb7ed839c95f
stefrvg <PS181901@edu.summacollege.nl>
2019-05-13 09:39:10 +0200

    card update

--- werkvormen.php
+++ werkvormen.php
@@ -15,6 +15,28 @@
     include "include/nav.html";
     include "include/header.html";
     ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+        <div class="card">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card-block">
+                        <h4 class="card-title">Curabitur gravida vestibulum imperdiet.</h4>
+                            <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem. Integer sed mi quis nisl eleifend interdum.</p>
+                            <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem.</p>
+
+                    </div>
+                    </div>
+                    <div class="col-md-3">
+                         <div class="card-img-bottom">
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php
     include "include/footer.html";
     ?>
