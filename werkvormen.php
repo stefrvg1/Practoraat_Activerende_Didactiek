f0bc1d5c65da461b7c9ab0468bb06580e5082049
stefrvg <PS181901@edu.summacollege.nl>
2019-05-13 09:04:06 +0200

    nieuwe bootstrap

--- werkvormen.php
+++ werkvormen.php
@@ -1,40 +1,22 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<?php
-
-include "include/head.html";
-
-?>
-<body>
-<?php
-include "include/nav.html";
-include "include/header.html";
-?>
-
-  <!-- Main Content -->
-
-<div class="container">
-    <div class="row">
-        <div class="col-lg-3">
-            <h4>Filter ></h4>
-        </div>
-        <div class="col-log-9">
-            <h4>Werkvormen</h4>
-        </div>
-    </div>
-</div>
-
-<!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Custom scripts for this template -->
-<script src="js/clean-blog.min.js"></script>
-
-<?php
-include "include/footer.html";
-?>
-
-</body>
-
+  <head>
+    <title>Werkvormen</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php
+    include "include/head.html";
+    ?>
+    
+  </head>
+  <body>
+    <?php
+    include "include/nav.html";
+    include "include/header.html";
+    ?>
+    <?php
+    include "include/footer.html";
+    ?>
+  </body>
 </html>
