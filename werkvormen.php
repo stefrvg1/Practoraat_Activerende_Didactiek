216e0d4aab7e15555d4e44e8d8d51a8f224a905c
stefrvg <PS181901@edu.summacollege.nl>
2019-05-13 13:05:22 +0200

    werkvormen pagina geupdate

--- werkvormen.php
+++ werkvormen.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <title>Werkvormen</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,37 +8,157 @@
     <?php
     include "include/head.html";
     ?>
-    
-  </head>
-  <body>
-    <?php
-    include "include/nav.html";
-    include "include/header.html";
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-            </div>
-        <div class="card">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card-block">
-                        <h4 class="card-title">Curabitur gravida vestibulum imperdiet.</h4>
-                            <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem. Integer sed mi quis nisl eleifend interdum.</p>
-                            <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem.</p>
 
-                    </div>
-                    </div>
-                    <div class="col-md-3">
-                         <div class="card-img-bottom">
-                         </div>
-                    </div>
+</head>
+<body>
+<?php
+include "include/nav.html";
+include "include/header.html";
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-2">
+            <table class="table" style="font-size: 15px">
+                <thead>
+                <tr>
+                    <th>Filter</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="2">
+                    </td>
+                    <td>
+                        <label>Kwaliteitenreflectie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="3">
+                    </td>
+                    <td>
+                        <label>Motievenreflectie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="4">
+                    </td>
+                    <td>
+                        <label>Werkexploratie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="1">
+                    </td>
+                    <td>
+                        <label>Netwerken</label>
+                    </td>
+                </tr>
+                <tr style="visibility: hidden">
+                    <td scope="row">
+                        <input type="checkbox" id="1">
+                    </td>
+                    <td>
+                        <label>Netwerken</label>
+                    </td>  
+                </tr>
+                </tbody>
+            </table>
+            
+        </div>
+        <div class="col-1">
+            <table class="table" style="margin-top: 268px; margin-left: -130px; font-size: 13px;">
+                <tbody>
+                <tr>
+                    <td>
+                        <input type="checkbox" id="2">
+                    </td>
+                    <td>
+                        <label>Kwaliteitenreflectie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="3">
+                    </td>
+                    <td>
+                        <label>Motievenreflectie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="4">
+                    </td>
+                    <td>
+                        <label>Werkexploratie</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td scope="row">
+                        <input type="checkbox" id="1">
+                    </td>
+                    <td>
+                        <label>Netwerken</label>
+                    </td>
+                </tr>
+                <tr style="visibility: hidden">
+                    <td scope="row">
+                        <input type="checkbox" id="1">
+                    </td>
+                    <td>
+                        <label>Netwerken</label>
+                    </td>  
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-9">
+            <!-- <script>
+                        var text = "";
+                        var i = 0;
+                        while (i < 10) {
+                          text += "<br>The number is " + i;
+                          i++;
+                        }
+                        document.getElementById("demo").innerHTML = text;
+                </script> -->
+                
+            <div class="card" style="margin-left: 70px; margin-top: 20px; margin-bottom: 20px; width: 800px; height: 450px;">
+                <img class="card-img-top" src="img/home.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Werkvorm</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero blanditiis maxime aliquid non eum. Dolore, illo? Error cumque perferendis beatae. Error maiores minima repellendus id aperiam reprehenderit explicabo debitis ipsa?</p>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 70px; margin-top: 20px; margin-bottom: 20px; width: 800px; height: 450px;"> 
+                <div class="card-body">
+                    <h4 class="card-title">Werkvorm</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero blanditiis maxime aliquid non eum. Dolore, illo? Error cumque perferendis beatae. Error maiores minima repellendus id aperiam reprehenderit explicabo debitis ipsa?</p>
+                    <img class="card-img-top" src="img/home.jpg" alt="">
+                </div>
+            </div>
+            <div class="card" style="margin-left: 70px; margin-top: 20px; margin-bottom: 20px; width: 800px; height: 450px;">
+                <img class="card-img-top" src="img/home.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Werkvorm</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero blanditiis maxime aliquid non eum. Dolore, illo? Error cumque perferendis beatae. Error maiores minima repellendus id aperiam reprehenderit explicabo debitis ipsa?</p>
+                </div>
+            </div>
+            <div class="card" style="margin-left: 70px; margin-top: 20px; margin-bottom: 20px; width: 800px; height: 450px;">
+                <img class="card-img-top" src="img/logo.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Werkvorm</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero blanditiis maxime aliquid non eum. Dolore, illo? Error cumque perferendis beatae. Error maiores minima repellendus id aperiam reprehenderit explicabo debitis ipsa?</p>
                 </div>
             </div>
         </div>
     </div>
-    <?php
-    include "include/footer.html";
-    ?>
-  </body>
+</div>
+<?php
+include "include/footer.html";
+?>
+</body>
 </html>
